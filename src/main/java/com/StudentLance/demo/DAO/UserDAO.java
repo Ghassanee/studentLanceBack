ffc6a84c221f1704ba85344205ca31063668a8f3
Ghassanee <ghassane20a@gamil.com>
2020-12-06 17:25:05 +0100

    message this

--- src/main/java/com/StudentLance/demo/DAO/UserDAO.java
+++ src/main/java/com/StudentLance/demo/DAO/UserDAO.java
@@ -13,9 +13,9 @@ public interface UserDAO extends JpaRepository<User,Long> {
 
     public User findByuserId(int id);
 
-    public User findUserByPhone(@Param("phone") String phone);
+    public User findUserByPhone(String phone);
 
-    public User findUserByEmail(@Param("email") String email);
+    public User findUserByEmail String email);
 
 }
 
