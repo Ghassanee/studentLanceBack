f8b5ccc811754903a03f25f1daab2de157b3056c
Ghassanee <ghassane20a@gamil.com>
2020-12-07 00:49:23 +0100

    commit 3

--- src/main/java/com/StudentLance/demo/DAO/CompanyDAO.java
+++ src/main/java/com/StudentLance/demo/DAO/CompanyDAO.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 @Repository
-public interface CompanyDAO extends JpaRepository<Company,Integer> {
+public interface CompanyDAO extends JpaRepository<Company,Long> {
 
     public Company findByCompanyId(int  companyId);
     public List<Company> findAll();
